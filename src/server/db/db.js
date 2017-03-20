1e36b1088c34cd3b1db87a6514c0683bf18c6a24
Kongfujonstew <jonstew@gmail.com>
2017-03-20 11:38:03 -0700

    digital ocean connection, add schema to db.js

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var mongodb = require('mongodb');
-
+//
 mongoose.Promise = Promise;
 
-mongoURI = 'mongodb://localhost/vizly'; 
+mongoURI = 'mongodb://root:Go_Vizly@138.197.206.253:27107/Vizly';
+//'mongodb://0.0.0.0:27017/vizly'; 
 mongoose.connect(mongoURI);
 
-
-//mongo "mongodb://mycluster0-shard-00-00-wpeiv.mongodb.net:27017/admin?replicaSet=Mycluster0-shard-0" --ssl --TeamVizly kay --password HR72Go_Vizly!
+////mongo "mongodb://mycluster0-shard-00-00-wpeiv.mongodb.net:27017/admin?replicaSet=Mycluster0-shard-0" --ssl --TeamVizly kay --password HR72Go_Vizly!
 
 
 
@@ -19,9 +19,12 @@ db.once('open', function () {
 });
 
 var Schema = mongoose.Schema({
-  userName: String,
+  name: String,
   email: String,
-  picsUrl: String
+  username: String,
+  provider: String,
+  facebook: Object,
+  pics: Array
 });
 
 var User = db.model('User', Schema);
