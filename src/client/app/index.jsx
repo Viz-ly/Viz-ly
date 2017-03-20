6e004e7c5386d20440499d7bcef0e4b850d55a70
ojolao <jolaosho@Omotayos-MBP.attlocal.net>
2017-03-19 23:08:06 -0700

    create login page
    with facebook log in using passportjs

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+const iconStyle = {
+  width: 40,
+  height: 40
+};
+
+
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <p> Hello Viz.ly!</p>
-        
+      <div style={{paddingLeft: 25}}>
+        <h1>Viz.ly </h1>
+          <div style={{display: 'inline-block', width: '60%', paddingTop: 100, paddingRight: 10 }}>
+            <h2> A picture is worth a thousand words. What do your pictures say about you?</h2>
+            <p> At Viz.ly, we apply a sophisticated algorithm to your photo collection, resulting in a dynamic visual profile of who you are that you can share with the world. Getting started is easy:</p>
+            <ul style={{marginBottom: 30}}>
+              <p><img src={require('../../../my-icons-collection/png/002-photo-slideshow.png')} style={iconStyle} />   Upload your collection of photographs</p>
+              <p><img src={require('../../../my-icons-collection/png/003-color-control.png')} style={iconStyle} />  Explore your results using our interactive visualization</p>
+              <p><img src={require('../../../my-icons-collection/png/007-camera-portrait-mode.png')} style={iconStyle} />  Share who you are with the world by uploading your results to your social media feeds</p>
+            </ul>
+            <a href="/auth/facebook" className="btn btn-primary" ><span className="fa fa-facebook"></span> Facebook Sign Up</a>
+          </div>
+            <img src={require('../../../my-icons-collection/png/011-camera-and-photo-album.png')} style={{display: 'inline-block', maxWidth: '35%', paddingTop: 60, verticalAlign: 'top' }} />
       </div>
     );
   }
--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+//var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var db = require('./db/db.js');
 var User = db.User;
-
-
-var app = express();
-
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var configAuth = require('../client/env/config.js');
 
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+var app = express();
+//app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', express.static(__dirname + '/../client'));
+
+
 
 passport.use(new FacebookStrategy({
   clientID: configAuth.facebookAuth.clientID,
@@ -21,16 +40,21 @@ passport.use(new FacebookStrategy({
 },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({'userName': profile.id}, function(err, user) {
-      if (err) { return done(err); }
+      if (err) { 
+        console.log(err);
+        return done(err); 
+      }
       //if no user was found, create a new user with values from Facebook
       if (!user) {
+        console.log(profile);
         user = new User({
           name: profile.displayName,
-          email: profile.emails[0].value,
-          username: profile.userName,
+          email: 'tbd',
+          username: profile.userName || profile.id,
           provider: 'facebook',
           facebook: profile._json
         });
+        console.log(user);
         user.save(function(err) {
           if (err) { console.log(err); }
           return done(err, user);
